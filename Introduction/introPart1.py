fb72e531678bc53684c81f22edf6ae3cb9531bf0
Deep Parmar <deep56parmar@hotmail.com>
2018-07-20 21:16:12 +0530

    input function and Control Characteres.

---
+++ Introduction/introPart1.py
@@ -0,0 +1,18 @@
+# Created By: Deep Parmar ('deep56parmar@hotmail.com')
+# Date :  09/07/2018
+# Introduction Part 1
+string = 'Hello'
+name = input('Please Enter Your Name: ') 
+# Input Function for taking input from keyboard using console. 
+print(string + ' ' + name)
+
+# Control characters 
+splitString = 'This is String.\nThis is new line.' 
+print(splitString)
+
+tabbedString = 'This\tis\ttabbed\tstring'
+print(tabbedString)
+
+multiLineString = """ Multilne string 
+Yes, Python is amazing isn\'t it? """
+print(multiLineString)
