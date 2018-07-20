20a27cfc8a679d0d0bbb042b5d010d78f12ce630
Deep Parmar <deep56parmar@hotmail.com>
2018-07-20 21:16:45 +0530

    introPart 1

--- Introduction/introPart1.py
+++ Introduction/introPart1.py
@@ -1,6 +1,6 @@
 # Created By: Deep Parmar ('deep56parmar@hotmail.com')
 # Date :  09/07/2018
-# Introduction Part 1
+# Introduction Part 1 (Input function and control characters)
 string = 'Hello'
 name = input('Please Enter Your Name: ') 
 # Input Function for taking input from keyboard using console. 
