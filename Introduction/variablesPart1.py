518f4b2b26f3621975c9a74a1d897f8b7f9e275d
Deep Parmar <deep56parmar@hotmail.com>
2018-07-20 21:41:45 +0530

    variablesPart1

---
+++ Introduction/variablesPart1.py
@@ -0,0 +1,24 @@
+# Created By: Deep Parmar ('deep56parmar@hotmail.com')
+# Date :  20/07/2018
+# Variables Part 1 (Valid Ways to name a variables, Numeric Data types)
+
+variable = "Deep" 
+# 'variable' is a variabel and Deep is value
+Variable = 'Valid'
+_variable = 'Valid'
+variable123 = 'Valid'
+# Valid ways to name a variables. 
+
+a = 123
+b = 123 
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b) # Floor division
+print(a % b)
+# Expressions
+
+print(a + b / 3 - 2 + 12 )
+# Remember result depends on operator precedence.
+
