2cd07c95f5eb17ed2e0ece851739ad41d603d8bf
Deep Parmar <deep56parmar@hotmail.com>
2018-07-20 22:33:14 +0530

    VariablesPart1 part 2 and replacement fields

---
+++ Introduction/variablesPart2.py
@@ -0,0 +1,17 @@
+# Created By: Deep Parmar ('deep56parmar@hotmail.com')
+# Date :  20/07/2018
+# Variables Part 2 (Strings and in operator)
+
+dog = 'Labradore Black'
+print(dog)
+print(dog[0]) # First charecter of string dog
+print(dog[3]) # 4th Charecter of string dog
+print(dog[-1]) # Last charecter
+print(dog[0:6]) # From first to sixth charecter
+print(dog[6:]) # From sixth to last charecter
+print(dog[0:10:2]) # From first to tenth with gap of one charecters.
+
+print('Hello' * 5) # Prints Hello 5 times.
+print("day" in 'today') # Returns true as day is substring of today
+
+
---
+++ Introduction/variablesPart3.py
@@ -0,0 +1,11 @@
+# Created By: Deep Parmar ('deep56parmar@hotmail.com')
+# Date :  20/07/2018
+# Variables Part 3 (Strings and Function)
+
+age = 22
+print('My age is ' + str(age) + ' years.') # str() funtion convert any datatype to string.
+
+# Replacement Fields
+print('My age is {0} years.'.format(age) ) # format() use for formatting strings using multiple arguments.
+print('There are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, {7}, '.format(31, "January", "March", "May", "July", "August", "October", "December"))
+print('Pi is approximately {0:12.50f} '.format(22/7))
